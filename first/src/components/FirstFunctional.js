f0ba59113f66299801dc8f0473d45f57045c9b02
Adam Fetters <adamfetterscoder@gmail.com>
2018-02-20 12:10:30 -0500

    Functional_Component

---
+++ first/src/components/FirstFunctional.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstFunctional = () => {
+  return (
+    <div>
+      <h1>First Functional Component</h1>
+    </div>
+  );
+};
+
+export default FirstFunctional;
