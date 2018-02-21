415ecad7c9fd16b889295ff1ff0a4fc34c8b6fb4
Adam Fetters <adamfetterscoder@gmail.com>
2018-02-21 12:15:26 -0500

    Working react router and link

---
+++ first/src/components/05/Client-Side-Routing-master/Client-Side-Routing-master/client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './index.css';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" exact component={MovieList} />
+      {/* <Route path="/movies" component={Movie} /> */}
+      <Route path="/movies/:id" component={Movie} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
